5a0ac17aea6c11cadfc06cebeaa94b0024b28215
Certil Remy <certil@noukod.com>
2019-02-05 16:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Write your solution in this file!
-
+var customerName = 'bob' ;
 function upperCaseCustomerName()
 {
-  var customerName = 'bob' ;
   return customerName.toUpperCase();
 }
 upperCaseCustomerName();
